1e50b43345ff8ad36629f8c7d9f2af92a0eab4a9
HeeWon Lee <ncross42@gmail.com>
2018-05-09 02:17:07 +0900

    upgrade packages

--- build/build.js
+++ build/build.js
@@ -3,18 +3,18 @@ require('./check-versions')()
 
 process.env.NODE_ENV = 'production'
 
-const ora = require('ora')
-const rm = require('rimraf')
-const path = require('path')
-const chalk = require('chalk')
-const webpack = require('webpack')
-const config = require('../config')
-const webpackConfig = require('./webpack.prod.conf')
+import ora from "ora";
+import rm from "rimraf";
+import { join } from "path";
+import { red, cyan, yellow } from "chalk";
+import webpack from "webpack";
+import { build } from "../config";
+import webpackConfig from "./webpack.prod.conf";
 
 const spinner = ora('building for production...')
 spinner.start()
 
-rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
+rm(join(build.assetsRoot, build.assetsSubDirectory), err => {
   if (err) throw err
   webpack(webpackConfig, (err, stats) => {
     spinner.stop()
@@ -28,12 +28,12 @@ rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
     }) + '\n\n')
 
     if (stats.hasErrors()) {
-      console.log(chalk.red('  Build failed with errors.\n'))
+      console.log(red('  Build failed with errors.\n'))
       process.exit(1)
     }
 
-    console.log(chalk.cyan('  Build complete.\n'))
-    console.log(chalk.yellow(
+    console.log(cyan('  Build complete.\n'))
+    console.log(yellow(
       '  Tip: built files are meant to be served over an HTTP server.\n' +
       '  Opening index.html over file:// won\'t work.\n'
     ))
--- src/permission.js
+++ src/permission.js
@@ -1,11 +1,11 @@
 import router from './router'
 import store from './store'
-import NProgress from 'nprogress' // Progress 进度条
-import 'nprogress/nprogress.css'// Progress 进度条样式
+import NProgress from 'nprogress' // Progress 표시줄
+import 'nprogress/nprogress.css'// Progress 표시줄스타일
 import { Message } from 'element-ui'
-import { getToken } from '@/utils/auth' // 验权
+import { getToken } from '@/utils/auth' // 확인 권한
 
-const whiteList = ['/login'] // 不重定向白名单
+const whiteList = ['/login'] // 허용 목록을 리디렉션하지 않습니다.
 router.beforeEach((to, from, next) => {
   NProgress.start()
   if (getToken()) {
@@ -14,7 +14,7 @@ router.beforeEach((to, from, next) => {
       NProgress.done() // if current page is dashboard will not trigger	afterEach hook, so manually handle it
     } else {
       if (store.getters.roles.length === 0) {
-        store.dispatch('GetInfo').then(res => { // 拉取用户信息
+        store.dispatch('GetInfo').then(res => { // 사용자 정보 가져 오기
           next()
         }).catch((err) => {
           store.dispatch('FedLogOut').then(() => {
